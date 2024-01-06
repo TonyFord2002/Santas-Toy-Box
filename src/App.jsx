27eff11cbff984b86ff82622e40cb2e33d82c560
TonyFord2002 <97993837+TonyFord2002@users.noreply.github.com>
2024-01-06 09:49:30 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import santasToyBoxLogo from './santasToyBoxLogo.png'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={santasToyBoxLogo} className="App-logo" alt="logo" />
+        <p className='Elizabeth'>
+        Elizabeth is awesome!! She will do amazing in her performance!!
+        </p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
